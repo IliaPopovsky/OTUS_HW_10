f34c43459ba808a1fd65188fb4f21f00b38d71ee
Ilia Popovsky <popovskyilia@gmail.com>
2024-10-17 20:45:32 +0300

    first version prodject

--- daemon_for_size_files.c
+++ daemon_for_size_files.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(void)
 {
+    FILE *fp = NULL;
+    long last = 0;
+
+    if((fp = fopen("control_file.txt", "rb")) == NULL)
+    {
+       printf("Not open file %s\n", "control_file.txt");
+       exit(EXIT_FAILURE);
+    }
+    fseek(fp, 0L, SEEK_END);               // перейти в конец файла
+    last = ftell(fp);
+    printf("last = %ld\n", last);
     printf("Hello world!\n");
     return 0;
 }
