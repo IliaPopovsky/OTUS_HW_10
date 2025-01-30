b22cca627c3b1cf8a7d0e427b31e9043f777c4d2
Ilia Popovsky <popovskyilia@gmail.com>
2025-01-30 17:41:14 +0300

    first version project1

--- Server/daemon_for_size_files_server.c
+++ Server/daemon_for_size_files_server.c
@@ -1,7 +1,10 @@
+// daemon_for_size_files_server.c
 #include "unp.h"
 
 #include <stdarg.h>
 #include <syslog.h>		                                                /* for syslog() */
+
+#include <sys/resource.h>
 
 int daemon_proc;		                                                /* set nonzero by daemon_init() */
 
@@ -138,28 +141,29 @@ Writen(int fd, void *ptr, size_t nbytes)
 		err_sys("writen error");
 }
 
-void str_echo1(int sockfd, long size)
+void responce_size_file(int sockfd, long size)
 {
    ssize_t n;
    char buf[MAXLINE];
    memset(buf, 0, MAXLINE);
-   //long arg1, arg2;
+   snprintf(buf, sizeof(buf), "%ld\n", size);
+   n = strlen(buf); 
+   Writen(sockfd, buf, n); 
+   printf("Ответ сервера отправлен!\n"); 
+   return;
+}
+
+void mistake_open_file_for_clients(int sockfd, char *array)
+{
+   ssize_t n;
+   char buf[MAXLINE];
+   memset(buf, 0, MAXLINE);
+   snprintf(buf, sizeof(buf), "Не удается открыть файл %s\n", array);
+   n = strlen(buf); 
+   Writen(sockfd, buf, n); 
+   printf("Ответ сервера отправлен!\n"); 
+   return;
    
-   for(;;){
-     //if( (n = Readline(sockfd, buf, MAXLINE)) == 0)
-        //return;            /* соединение закрыто с другого конца */
-        
-     //if(sscanf(buf, "%ld%ld", &arg1, &arg2) == 2)
-         //snprintf(buf, sizeof(buf), "%ld\n", arg1 + arg2);
-     //else
-         //snprintf(buf, sizeof(buf), "input error\n");
-         
-     snprintf(buf, sizeof(buf), "%ld\n", size);
-     n = strlen(buf); 
-     Writen(sockfd, buf, n); 
-     printf("Ответ сервера отправлен!\n"); 
-     return;
-   }
 }
 
 void sig_chld(int signo)
@@ -203,60 +207,9 @@ Sigfunc *Signal(int signo, Sigfunc *func)	/* for our signal() function */
 	return(sigfunc);
 }
 
-static ssize_t my_read(int fd, char *ptr)
-{
-	static int	read_cnt = 0;
-	static char	*read_ptr;
-	static char	read_buf[MAXLINE];
 
-	if (read_cnt <= 0) {
-again:
-		if ( (read_cnt = read(fd, read_buf, sizeof(read_buf))) < 0) {
-			if (errno == EINTR)
-				goto again;
-			return(-1);
-		} else if (read_cnt == 0)
-			return(0);
-		read_ptr = read_buf;
-	}
 
-	read_cnt--;
-	*ptr = *read_ptr++;
-	return(1);
-}
-
-ssize_t readline(int fd, void *vptr, size_t maxlen)
-{
-	int		n, rc;
-	char	c, *ptr;
-
-	ptr = vptr;
-	for (n = 1; n < maxlen; n++) {
-		if ( (rc = my_read(fd, &c)) == 1) {
-			*ptr++ = c;
-			if (c == '\n')
-				break;	/* newline is stored, like fgets() */
-		} else if (rc == 0) {
-			if (n == 1)
-				return(0);	/* EOF, no data read */
-			else
-				break;		/* EOF, some data was read */
-		} else
-			return(-1);		/* error, errno set by read() */
-	}
-
-	*ptr = 0;	/* null terminate like fgets() */
-	return(n);
-}
-
-ssize_t Readline(int fd, void *ptr, size_t maxlen)
-{
-	ssize_t		n;
-
-	if ( (n = readline(fd, ptr, maxlen)) < 0)
-		err_sys("readline error");
-	return(n);
-}
+void daemonize(const char* cmd);
 
 int main(int argc, char **argv)
 {
@@ -264,14 +217,34 @@ int main(int argc, char **argv)
    pid_t childpid;
    socklen_t clilen, servlen;
    struct sockaddr_un cliaddr, servaddr;
-   char buff[MAXLINE];
-   //char sock_file[] = "/tmp/daemon_socket";
    char sock_file[300] = {0};
    char name_file[300] = {0};
    long size = 0;
    FILE *fp = NULL;
    FILE *fpt = NULL;
    char ch = 0;
+   char control_symbol = 0;
+   char *cmd;
+   struct sigaction sa;
+   
+   if(argc < 2)
+       err_quit("Enter the absolute pathname of the file as the first argument to this program!");
+   if ((cmd = strrchr(argv[0], '/')) == NULL)
+       cmd = argv[0];
+   else
+       cmd++;
+   printf("cmd = %s\n", cmd);
+   printf("Вы хотите демонизировать (перевести в фоновый режим работы) наш процесс с PID = %d?\n", getpid());
+   printf("Введите y если ДА, введите n (или любой другой символ отличный от y) если НЕТ:\n");
+   scanf("%c", &control_symbol);
+   while(getchar() != '\n')
+       continue;
+
+   if(control_symbol == 'y')
+   {
+       daemonize(cmd);
+   }
+   
    if( (fp = fopen(argv[1], "r")) == NULL)
    {
       fprintf(stderr, "Не удается открыть файл %s\n", argv[1]);
@@ -290,50 +263,17 @@ int main(int argc, char **argv)
       }
    }
    printf("name_file = $%s$\n", name_file);
-   #if 0
-   if( (fpt = fopen(name_file, "r")) == NULL)
-   {
-       printf("Not open file %s\n", name_file);
-       exit(EXIT_FAILURE);
-   }
-   #endif
-   //fseek(fpt, 0L, SEEK_END);               // перейти в конец файла
-   //size = ftell(fpt);
-   //char sock_file[] = argv[1];
-   
    
    unlink(sock_file);
    
-   listenfd = Socket(AF_UNIX, SOCK_STREAM, 0);                         // Создание сокета. Функция socket() создает объект сокета в ядре ОС с помощью системного вызова socket, который имеет номер
-                                                                       // 41 для 64-битной ОС Linux и номер 359 для 32-битной ОС LINUX, а в других ОС какой-то другой номер.
-                                                                       // Системный вызов socket() создаёт конечную точку соединения и возвращает файловый дескриптор, указывающий на эту точку. 
-                                                                       // Возвращаемый при успешном выполнении файловый дескриптор будет иметь самый маленький номер, который не используется 
-                                                                       // процессом.                                     
-   bzero(&servaddr, sizeof(servaddr));                                 // Инициализируем всю структуру адреса сервера нулями. Функция bzero() имеет свою реализацию в некоторых ОС. У нас она
-                                                                       // имеет вид функционального макроса, который вызывает библиотечную функцию memset() и располагается в файле unp.h
-                                                                       // на случай, если в какой-то ОС не будет своей реализации функции bzero().
-   servaddr.sun_family = AF_UNIX;                                      // Присваиваем значение полю структуры адреса сервера, отвечающему за семейство адресов.
+   listenfd = Socket(AF_UNIX, SOCK_STREAM, 0);                                                           
+   bzero(&servaddr, sizeof(servaddr));                                 
+   servaddr.sun_family = AF_UNIX;                                      
    strncpy(servaddr.sun_path, sock_file, sizeof(servaddr.sun_path) - 1);
    
-   Bind(listenfd, (SA *)&servaddr, sizeof(servaddr));                  // Связывание адреса сервера с сокетом. Функция bind() использует системный вызов bind, который имеет номер
-                                                                       // 49 для 64-битной ОС Linux и номер 361 для 32-битной ОС LINUX, а в других ОС какой-то другой номер.
-                                                                       // Когда сокет создается с помощью socket(), он существует в пространстве имен (семействе адресов), но ему не присвоен адрес.  
-                                                                       // Функция bind() присваивает адрес, указанный параметром servaddr, сокету, на который ссылается файловый дескриптор listenfd.
-                                                                       // sizeof(servaddr) определяет размер в байтах структуры адреса, на которую указывает servaddr. Традиционно эта операция     
-                                                                       // называется «присвоение имени сокету». 
-                                                                       // Обычно, сокету типа SOCK_STREAM нужно назначить локальный адрес с помощью bind() до того, как он сможет принимать соединения.
-   Listen(listenfd, LISTENQ);                                          // Преобразование сокета в прослушиваемый, т.е. такой, на котором ядро принимает входящие соединения от клиетов.
-                                                                       // Функция listen() использует системный вызов listen, который имеет номер 50 для 64-битной ОС Linux и номер 363 для 32-битной
-                                                                       // ОС LINUX, а в других ОС какой-то другой номер.
-                                                                       // Вызов listen() помечает сокет, указанный в listenfd как пассивный, то есть как сокет, который будет использоваться для приёма
-                                                                       // запросов входящих соединений с помощью accept(). Аргумент listenfd является файловым дескриптором, который ссылается на сокет
-                                                                       // типа SOCK_STREAM.
-                                                                       // Константа LISTENQ задает максимальное количество клиентских соединений, которые ядро ставит в очередь на прослушиваемом
-                                                                       // сокете. Если приходит запрос на соединение, а очередь полна, то клиент может получить ошибку с указание ECONNREFUSED или,
-                                                                       // если низлежащий протокол поддерживает повторную передачу, запрос может быть игнорирован, чтобы попытаться соединиться
-                                                                       // позднее.
-                                                                       // Эти три этапа, socket, bind и listen, обычны для любого сервера TCP при создании того, что мы называем
-                                                                       // прослушиваемым дескриптором (listening descriptor) (в нашем случае это переменная listenfd). 
+   Bind(listenfd, (SA *)&servaddr, sizeof(servaddr));                  
+                                                                       
+   Listen(listenfd, LISTENQ);                                          
    
    Signal(SIGCHLD, sig_chld);           /* нужно вызвать waitpid() */                                                                   
    printf("Наш сервер работает!\n");
@@ -347,19 +287,7 @@ int main(int argc, char **argv)
    for(;;){
       printf("Цикл нашего сервера работает! (До вызова accept())\n");
       clilen = sizeof(cliaddr); 
-      if( (connfd = Accept(listenfd, (SA *) &cliaddr, &clilen)) < 0){  // Прием клиентского соединения. Функция accept() использует системный вызов accept, который имеет номер
-                                                                       // 43 для 64-битной ОС Linux, а в 32-битной ОС LINUX его нет, а в других ОС какой-то другой номер.
-                                                                       // Системный вызов accept используется с типами сокетов, основанными на соединении (SOCK_STREAM, SOCK_SEQPACKET).
-                                                                       // Он извлекает первый запрос на соединение из очереди ожидающих соединений для прослушивающего сокета, listenfd, создаёт новый
-                                                                       // подключенный сокет и возвращает новый файловый дескриптор, указывающий на сокет. Новый сокет более не находится в слушающем
-                                                                       // состоянии. Исходный сокет listenfd не изменяется при этом системном вызове.
-                                                                       // Аргумент listenfd - сокет, который был создан с помощью socket(), привязанный к локальному адресу с помощью bind() и
-                                                                       // прослушивающий соединения после listen().
-                                                                       // Обычно процесс сервера блокируется при вызове функции accept(), ожидая принятия подключения клиента. Для установки 
-                                                                       // TCP-соединения используется трех-этапное рукопожатие (three-way- handshake). Когда рукопожатие состоялось, функция accept()
-                                                                       // возвращает значение, и это значение является новым дескриптором (connfd), который называется присоединенным дескриптором
-                                                                       // (connected descriptior). Этот новый дескриптор используется для связи с новым клиентом. Новый дескриптор возвращается
-                                                                       // функцией accept() для каждого клиента, соединяющегося с нашим сервером.
+      if( (connfd = Accept(listenfd, (SA *) &cliaddr, &clilen)) < 0){  
          if(errno == EINTR)
              continue;                      /* назад к for() */
          else
@@ -374,18 +302,83 @@ int main(int argc, char **argv)
        Close(listenfd);             /* закрываем прослушиваемый сокет */
        if( (fpt = fopen(name_file, "r")) == NULL)
        {
-          printf("Not open file %s\n", name_file);
+          mistake_open_file_for_clients(connfd, name_file);
+          printf("Not open file (message for server) %s\n", name_file);
           exit(EXIT_FAILURE);
        }
        fseek(fpt, 0L, SEEK_SET);               // перейти в начало файла
        fseek(fpt, 0L, SEEK_END);               // перейти в конец файла
        size = ftell(fpt);
-       str_echo1(connfd, size);            /* обрабатываем запрос */
+       responce_size_file(connfd, size);            /* обрабатываем запрос */
        exit(0);
      }
      Close(connfd);                 /* родительский процесс закрывает присоединенный сокет */
-     //unlink(sock_file);
    }
 }
- 
+
+void daemonize(const char* cmd)
+{
+           /*
+            * Инициализировать файл журнала.
+            */
+           openlog(cmd, LOG_CONS, LOG_DAEMON);
+           /*
+            * Сбросить маску режима создания файла.
+            */
+           umask(0);
+           /*
+            * Получить максимально возможный номер дескриптора файла.
+            */
+           struct rlimit rl;
+           if (getrlimit(RLIMIT_NOFILE, &rl) < 0)
+               perror("невозможно получить максимальный номер дескриптора");
+           /*
+            * Стать лидером нового сеанса, чтобы утратить управляющий терминал.
+            */
+           pid_t pid;
+           if ((pid = fork()) < 0)
+               perror("ошибка вызова функции fork");
+           else if (pid != 0) /* родительский процесс */
+               exit(0);
+           setsid();
+           /*
+            * Обеспечить невозможность обретения управляющего терминала в будущем.
+            */
+           struct sigaction sa;
+           sa.sa_handler = SIG_IGN;
+           sigemptyset(&sa.sa_mask);
+           sa.sa_flags = 0;
+           if (sigaction(SIGHUP, &sa, NULL) < 0)
+               syslog(LOG_CRIT, "невозможно игнорировать сигнал SIGHUP");
+           if ((pid = fork()) < 0)
+               syslog(LOG_CRIT, "ошибка вызова функции fork");
+           else if (pid != 0) /* родительский процесс */
+               exit(0);
+           /*
+            * Назначить корневой каталог текущим рабочим каталогом,
+            * чтобы впоследствии можно было отмонтировать файловую систему.
+            */
+           if (chdir("/") < 0)
+               syslog(LOG_CRIT, "невозможно сделать текущим рабочим каталогом /");
+           /*
+            * Закрыть все открытые файловые дескрипторы.
+            */
+           if (rl.rlim_max == RLIM_INFINITY)
+               rl.rlim_max = 1024;
+           for (int i = 0; i < rl.rlim_max; i++)
+               close(i);
+
+            /*
+             * Присоединить файловые дескрипторы 0, 1 и 2 к /dev/null.
+             */
+           int fd0 = open("/dev/null", O_RDWR);
+           int fd1 = dup(0);
+           int fd2 = dup(0);
+           if (fd0 != 0 || fd1 != 1 || fd2 != 2)
+           {
+              syslog(LOG_CRIT, "ошибочные файловые дескрипторы %d %d %d",fd0, fd1, fd2);
+              exit(1);
+           }
+
+} 
  
